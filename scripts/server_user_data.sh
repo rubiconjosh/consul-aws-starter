f8c938e0869c08da7e30b55ef65a0b673f980882
Joshua Grant <joshua.r.grant@outlook.com>
2022-05-07 14:16:53 -0700

    servers now install and starts consul

--- scripts/server_user_data.sh
+++ scripts/server_user_data.sh
@@ -4,3 +4,14 @@
 curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt-get update && apt-get install consul
+
+# Create server config
+cat > /etc/consul.d/consul.hcl <<EOF
+data_dir = "/opt/consul"
+server = true
+bootstrap_expect = 3
+EOF
+
+# Enable & Start Server
+systemctl enable consul
+systemctl start consul
